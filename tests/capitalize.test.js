d41b8dacaf7bb9183763e36d7744c8de0a6d1293
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-02-10 00:17:39 +0300

    refactor test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if(capitalize('hello') !== 'Hello'){
-    throw new Error('Функция работает неверно');
-}
+assert.strictEqual(capitalize("") === "")
+assert.strictEqual(capitalize('hello') === 'Hello')
 
-if(capitalize('') !== '') {
-    throw new Error('Функця работает неверно');
-}
 
 console.log('Все тесты пройдены')
