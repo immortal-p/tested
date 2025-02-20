1ada4b950d09bf23c7c252620f2c12704349417f
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-02-21 01:13:32 +0300

    update test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,24 @@
-import fs from 'fs';
+import fs, { readFile } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import reverse from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import reverse from '../src/index.js'
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+
+test('reverse', () => {
+      expect(reverse('kels')).toEqual('slek');
+      expect(reverse('')).toEqual('');
+})
+
+test('reverse with log text', () => {
+      const txt = readFixtureFile('data.txt').trim();
+      const expected = readFixtureFile('result.txt').trim();
+
+      expect(reverse(txt)).toEqual(expected);
+})
